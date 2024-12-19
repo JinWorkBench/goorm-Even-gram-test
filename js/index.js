10b6269c556ed9749d9afda786c23876e20d882c
JinWorkBench <brilyent72@gmail.com>
2024-12-19 19:59:18 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -329,7 +329,7 @@ async function loadStories() {
 
       // 스토리 항목 클릭 시 이동 (userId 전달)
       storyItem.addEventListener("click", () => {
-        const url = `/story.html?userId=${story.userId}`;
+        const url = `./story.html?userId=${story.userId}`;
         window.location.href = url;
       });
       storyList.appendChild(storyItem);
